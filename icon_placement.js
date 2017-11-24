8d7fe5ae36654b674e8ccb8638d6e01e2fbac977
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-24 15:07:55 +0100

    Created new img-tag in google search result DOM to display the fundraiser-icon for each search result and appied style-settings to the icon by creating new css file and referencing it in the manifest.json file

--- icon_placement.js
+++ icon_placement.js
@@ -1,5 +1,11 @@
-var listOfSearchResults = document.getElementsByClassName("_Rm");
+var listOfUrls = document.getElementsByClassName("_Rm");
 
-for (var i = 0; i < listOfSearchResults.length; i++) {
-  console.log(listOfSearchResults[i].innerHTML);
+for (var i = 0; i < listOfUrls.length; i++) {
+  var searchResultBlock = listOfUrls[i].parentElement.parentElement.parentElement.parentElement;
+
+  var icon = document.createElement("img");
+  icon.className = "fundraiser-icon";
+  icon.src = "http://www.bellevuehighptsa.com/wp/wp-content/uploads/2016/03/icon_grant.png";
+
+  searchResultBlock.appendChild(icon);
 }
