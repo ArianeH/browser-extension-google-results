8404a72f085f7712702050931cf3a17f4807dfb2
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-24 16:09:43 +0100

    Adjusted style settings of the search result DOM to display fundraiser icon left of each search result block

--- icon_placement.js
+++ icon_placement.js
@@ -1,11 +1,17 @@
 var listOfUrls = document.getElementsByClassName("_Rm");
 
 for (var i = 0; i < listOfUrls.length; i++) {
-  var searchResultBlock = listOfUrls[i].parentElement.parentElement.parentElement.parentElement;
+  var searchResultInfo = listOfUrls[i].parentElement.parentElement.parentElement;
+  var searchResultBlock = searchResultInfo.parentElement;
+  var parentOfSearchResultBlock = searchResultBlock.parentElement;
+  var linkToUrl = searchResultInfo.previousSibling.firstChild;
 
   var icon = document.createElement("img");
   icon.className = "fundraiser-icon";
   icon.src = "http://www.bellevuehighptsa.com/wp/wp-content/uploads/2016/03/icon_grant.png";
 
+  parentOfSearchResultBlock.classList.add("removed-margin");
+  searchResultInfo.classList.add("additional-margin");
+  linkToUrl.classList.add("additional-margin");
   searchResultBlock.appendChild(icon);
 }
