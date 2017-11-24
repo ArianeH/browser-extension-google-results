b63fa33f5bed6b7d9b042a7ccf0d8840668c144a
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-24 16:46:06 +0100

    Restricted display of fundraiser icon to search results of certain companies by comparing whether their name is included in the url

--- icon_placement.js
+++ icon_placement.js
@@ -5,13 +5,16 @@ for (var i = 0; i < listOfUrls.length; i++) {
   var searchResultBlock = searchResultInfo.parentElement;
   var parentOfSearchResultBlock = searchResultBlock.parentElement;
   var linkToUrl = searchResultInfo.previousSibling.firstChild;
+  var urlText = listOfUrls[i].innerHTML
 
   var icon = document.createElement("img");
   icon.className = "fundraiser-icon";
   icon.src = "http://www.bellevuehighptsa.com/wp/wp-content/uploads/2016/03/icon_grant.png";
 
-  parentOfSearchResultBlock.classList.add("removed-margin");
-  searchResultInfo.classList.add("additional-margin");
-  linkToUrl.classList.add("additional-margin");
-  searchResultBlock.appendChild(icon);
+  if ((urlText.indexOf("goertz") != -1) || (urlText.indexOf("schuhcenter") != -1)) {
+    parentOfSearchResultBlock.classList.add("removed-margin");
+    searchResultInfo.classList.add("additional-margin");
+    linkToUrl.classList.add("additional-margin");
+    searchResultBlock.appendChild(icon);
+  }
 }
