f5064b744cb50fb49c5ab9bbb9fef9d6ebf81ce1
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-25 20:11:13 +0100

    Refactored exisiting code to a main function to retrieve all urls and a nested function to add the icon to specific search results

--- icon_placement.js
+++ icon_placement.js
@@ -1,20 +1,25 @@
-var listOfUrls = document.getElementsByClassName("_Rm");
+window.onload = function getUrls(){
+  var listOfUrls = document.getElementsByClassName("_Rm");
 
-for (var i = 0; i < listOfUrls.length; i++) {
-  var searchResultInfo = listOfUrls[i].parentElement.parentElement.parentElement;
-  var searchResultBlock = searchResultInfo.parentElement;
-  var parentOfSearchResultBlock = searchResultBlock.parentElement;
-  var linkToUrl = searchResultInfo.previousSibling.firstChild;
-  var urlText = listOfUrls[i].innerHTML
+  for (var i = 0; i < listOfUrls.length; i++) {
+    var searchResultInfo = listOfUrls[i].parentElement.parentElement.parentElement;
+    var searchResultBlock = searchResultInfo.parentElement;
+    var wrapperOfSearchResultBlock = searchResultBlock.parentElement;
+    var linkToUrl = searchResultInfo.previousSibling.firstChild;
+    var urlText = listOfUrls[i].innerHTML;
 
-  var icon = document.createElement("img");
-  icon.className = "fundraiser-icon";
-  icon.src = "http://www.bellevuehighptsa.com/wp/wp-content/uploads/2016/03/icon_grant.png";
+    var icon = document.createElement("img");
+    icon.className = "fundraiser-icon";
+    icon.src = "http://www.bellevuehighptsa.com/wp/wp-content/uploads/2016/03/icon_grant.png";
+    addIconForFundraisers();
+  }
 
-  if ((urlText.indexOf("goertz") != -1) || (urlText.indexOf("schuhcenter") != -1)) {
-    parentOfSearchResultBlock.classList.add("removed-margin");
-    searchResultInfo.classList.add("additional-margin");
-    linkToUrl.classList.add("additional-margin");
-    searchResultBlock.appendChild(icon);
+  function addIconForFundraisers() {
+    if ((urlText.indexOf("goertz") != -1) || (urlText.indexOf("schuhcenter") != -1)) {
+      wrapperOfSearchResultBlock.classList.add("removed-margin");
+      searchResultInfo.classList.add("additional-margin");
+      linkToUrl.classList.add("additional-margin");
+      searchResultBlock.appendChild(icon);
+    }
   }
 }
